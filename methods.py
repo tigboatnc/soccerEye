542487a4a4d0380d66cdf69431710d0f60f4253b
Samarth Negi <tigboatnc@gmail.com>
2022-09-04 22:20:09 -0400

    added some parametric part

--- methods.py
+++ methods.py
@@ -6,10 +6,10 @@ def P_IF_1(frame,kernel_size=80,hsv_sens=140):
     # I = BGR 
     # O = Binary 
     
-    blurred = cv2.blur(frame, (kX, kY))
+    blurred = cv2.blur(frame, (kernel_size, kernel_size))
     
     frame_hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    lower_green = np.array([60 - hsv_sens, 100, 100])
+    lower_green = np.array([60 - hsv_sens, 40, 40])
     upper_green = np.array([60 + hsv_sens, 255, 255])
     
     mask = cv2.inRange(frame_hsv, lower_green, upper_green)
