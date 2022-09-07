bf84889cce804d6fcf96ba23158fe4a96940408f
Samarth Negi <tigboatnc@gmail.com>
2022-09-07 14:30:49 -0400

    added new pipeline for quantization

--- methods.py
+++ methods.py
@@ -14,4 +14,27 @@ def P_IF_1(frame,kernel_size=80,hsv_sens=140):
     
     mask = cv2.inRange(frame_hsv, lower_green, upper_green)
     return mask
+
+
+
+def colorQuantize(frame,div=64):
+    quantized = frame // div * div + div // 2
+    return quantized 
+
     
+    
+def P_IF_2(frame,kernel_size=80,hsv_sens=140,quant=100):
+    # I = BGR 
+    # O = Binary 
+    
+    blurred = cv2.blur(frame, (kernel_size, kernel_size))
+    frame_hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    lower_green = np.array([60 - hsv_sens, 100, 100])
+    upper_green = np.array([60 + hsv_sens, 255, 255])
+    
+    frame_hsv = colorQuantize(frame_hsv,div=quant)
+    
+    mask = cv2.inRange(frame_hsv, lower_green, upper_green)
+    return mask
+
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import cv2 
+import numpy as np
 
 def util_searching_all_files(directory: Path):   
     file_list = [] 
@@ -9,3 +11,14 @@ def util_searching_all_files(directory: Path):
         else:
             file_list.append(util_searching_all_files(directory/x))
     return file_list
+
+def stackImages(imgArray,axis=1):
+    imStack = []
+    for im in imgArray:
+
+        if len(im.shape) == 2:
+            imStack.append(cv2.cvtColor(im,cv2.COLOR_GRAY2RGB))
+        else:
+            imStack.append(im)
+            
+    return np.concatenate(imStack,axis=axis)
