cde50bf8d62e58a95ae4778640101993d59880d9
Samarth Negi <tigboatnc@gmail.com>
2022-09-07 23:45:01 -0400

    added pixel isolation method

--- methods.py
+++ methods.py
@@ -38,3 +38,39 @@ def P_IF_2(frame,kernel_size=80,hsv_sens=140,quant=100):
     mask = cv2.inRange(frame_hsv, lower_green, upper_green)
     return mask
 
+
+
+from skimage import exposure
+from skimage import data, img_as_float,img_as_ubyte
+def P_IF_3(frame,kernel_size=80,hsv_sens=20,pix_size=90):
+    # I = BGR 
+    # O = Binary 
+    
+    
+    blurred = cv2.blur(frame, (kernel_size, kernel_size))
+    
+    
+    # Desired "pixelated" size
+    w, h = (pix_size, pix_size)
+    height, width = blurred.shape[:2]
+    # Resize input to "pixelated" size
+    temp = cv2.resize(blurred, (w, h), interpolation=cv2.INTER_LINEAR)
+    # Initialize output image
+    frame_pixelated = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+    # Equalization
+    img_eq = img_as_ubyte(exposure.equalize_hist(frame_pixelated))
+    # img_adapteq = exposure.equalize_adapthist(frame_pixelated, clip_limit=0.03)
+    
+    frame_hsv = cv2.cvtColor(img_eq, cv2.COLOR_BGR2HSV)
+    
+
+
+    # # Adaptive Equalization
+   
+
+
+    lower_green = np.array([60 - hsv_sens, 40, 40])
+    upper_green = np.array([60 + hsv_sens, 255, 255])
+    mask = cv2.inRange(frame_hsv, lower_green, upper_green)
+    return mask
+    
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import cv2 
 import numpy as np
+from tqdm import tqdm
 
 def util_searching_all_files(directory: Path):   
     file_list = [] 
@@ -12,6 +13,7 @@ def util_searching_all_files(directory: Path):
             file_list.append(util_searching_all_files(directory/x))
     return file_list
 
+
 def stackImages(imgArray,axis=1):
     imStack = []
     for im in imgArray:
@@ -22,3 +24,66 @@ def stackImages(imgArray,axis=1):
             imStack.append(im)
             
     return np.concatenate(imStack,axis=axis)
+
+
+def resizeFrame(scale_percent,frame):
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+    
+    
+def applyToVideo(function,cap,skip=1,stack_original=False):
+    # Check if camera opened successfully
+    if (cap.isOpened() == False): 
+        print("Unable to read video")
+        return 
+    total_frames = cap.get(7)
+    if stack_original == False:
+        frame_width = int(cap.get(3))
+        frame_height = int(cap.get(4))
+        out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+        for framePick in tqdm(range(0,int(total_frames),skip)):
+            cap.set(1, framePick)
+            ret, frame = cap.read()
+            output = function(frame)
+            if len(output.shape) == 2 :
+                output = cv2.cvtColor(output, cv2.COLOR_GRAY2BGR)
+                out.write(output)
+            elif output.shape[2] == 3:
+                out.write(output)
+            else:
+                print('Misformed Format for writing to video')
+                break
+    else:
+        scale_percent = 50
+        frame_width_single = int(cap.get(3))
+        frame_height_single = int(cap.get(4))
+        
+        frame_width = int(frame_width_single *scale_percent  / 100) * 2 
+        frame_height = int(frame_height_single * scale_percent / 100)
+        
+        out = cv2.VideoWriter('output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+        for framePick in tqdm(range(0,int(total_frames),skip)):
+            cap.set(1, framePick)
+            ret, frame = cap.read()
+            output = function(frame)
+            if len(output.shape) == 2 :
+                output = cv2.cvtColor(output, cv2.COLOR_GRAY2BGR)
+                output_r = resizeFrame(50,output)
+                frame_r = resizeFrame(50,frame)
+                output = np.concatenate([output_r,frame_r],axis=1)
+                
+                out.write(output)
+            elif output.shape[2] == 3:
+                output_r = resizeFrame(scale_percent,output)
+                frame_r = resizeFrame(scale_percent,frame)
+                output = np.concatenate([output_r,frame_r],axis=1)
+                out.write(output)
+            else:
+                print('Misformed Format for writing to video')
+                break
+    out.release()
+
+
+    
